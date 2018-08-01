2b00ce817e0fe8c967841a399e6ed9c6e373986b
Colin Steffen <SteffenColin@t-online.de>
2018-08-01 16:48:38 +0200

    Xamarin Forms Serialiaztion Projekt

--- bm_ui_android_kotlin/app/src/main/java/de/colinsteffen/bm_ui_android_kotlin/ViewModel/UIBenchmarkViewModel.java
+++ bm_ui_android_kotlin/app/src/main/java/de/colinsteffen/bm_ui_android_kotlin/ViewModel/UIBenchmarkViewModel.java
@@ -19,7 +19,7 @@ public class UIBenchmarkViewModel extends ViewModel {
 
     public UIBenchmarkViewModel(){
 
-        for(int i = 0; i < 500; i++){
+        for(int i = 0; i < 1000; i++){
             InformationItems.add(new Information("Paderborn", "Bielefeld"));
         }
 
