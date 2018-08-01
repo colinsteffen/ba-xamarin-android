e9c7837a2bb2708319e799aa631539a4835a5dca
Colin Steffen <SteffenColin@t-online.de>
2018-08-01 17:31:51 +0200

    Xamarin Platform Serialiaztion Projekt

---
+++ bm_json_xam_platform/Shared/ViewModel/MainActivityViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Shared.ViewModel
+{
+    public class MainActivityViewModel
+    {
+        private List<Person> PersonList;
+        private string SerializedJsonList;
+
+        public MainActivityViewModel()
+        {
+            PersonList = new List<Person>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                PersonList.Add(new Person());
+            }
+        }
+
+        public void SerializePersons()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            SerializedJsonList = JsonConvert.SerializeObject(PersonList);
+
+            timer.Stop();
+            Debug.WriteLine("Time: Serialization -> " + timer.Elapsed);
+        }
+
+        public void DeserializePersons()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            PersonList = JsonConvert.DeserializeObject<List<Person>>(SerializedJsonList);
+
+            timer.Stop();
+            Debug.WriteLine("Time: Deserialization -> " + timer.Elapsed);
+            Debug.WriteLine("Time: Deserialization -> " + PersonList.Count);
+        }
+    }
+}
---
+++ bm_json_xam_platform/bm_json_xam_android/View/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Shared.ViewModel;
+
+namespace bm_json_xam_android
+{
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+        private MainActivityViewModel MainActivityVM;
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_main);
+
+            MainActivityVM = new MainActivityViewModel();
+
+			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            Button serializeBtn = FindViewById<Button>(Resource.Id.btn_serialize);
+            serializeBtn.Click += OnSerializedClicked;
+
+            Button deserializeBtn = FindViewById<Button>(Resource.Id.btn_deserialize);
+            deserializeBtn.Click += OnDeserializedClicked;
+        }
+
+		public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void OnSerializedClicked(object sender, EventArgs eventArgs)
+        {
+            MainActivityVM.SerializePersons();
+        }
+
+        private void OnDeserializedClicked(object sender, EventArgs eventArgs)
+        {
+            MainActivityVM.DeserializePersons();
+        }
+    }
+}
+
